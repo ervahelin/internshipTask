0511d163268839a7754d7606c166945c126824c9
Erva Helin Güyen <ervaguyen@gmail.com>
2025-03-18 22:48:58 +0100

    Part B: Base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import "./App.css";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <header>
+        <Nav />
+      </header>
+      <main></main>
+      <footer className="flex flex-col lg:flex-row gap-5 px-5 lg:px-10 py-5 lg:items-center lg:justify-between bg-slate-50">
+        <div className="h-10 w-10">
+          <a href="/">
+            <img src="logo.svg" alt="Logo"></img>
+          </a>
+        </div>
+        <div>
+          <ul className="lg:text-center">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col">
+          <span className="font-bold">Max Mustermann</span>
+          <span>Mariahilfer Straße 10</span>
+          <span>1070 Wien, Österreich</span>
+          <span>+43 660 1234567</span>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/nav.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-slate-50 px-5 lg:px-10 h-24 flex items-center justify-between">
+      <a href="/" className="h-10 w-10">
+        <img src="logo.svg" alt="Logo" />
+      </a>
+
+      {/* Burger Menu Icon */}
+      <button
+        className="lg:hidden block text-gray-700"
+        onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <X size={32} /> : <Menu size={32} />}
+      </button>
+
+      <div
+        className={`fixed top-24 right-0 w-full h-full bg-slate-50 transform transition-transform duration-300 lg:static lg:transform-none lg:flex lg:h-auto lg:w-auto ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}>
+        <ul className="flex flex-col lg:flex-row gap-10 p-10 lg:p-0 items-center  lg:justify-between h-full lg:h-auto">
+          <li>
+            <a href="#" className="block text-lg py-2 px-4">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block text-lg py-2 px-4">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block text-lg py-2 px-4">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
